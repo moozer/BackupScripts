bf1000bedfdabf080f197824058174445ea88afd
moz <moz@local>
2013-11-02 00:35:10 +0100

    Md5Db init added

--- backupscripts/SqfsMd5Db.py
+++ backupscripts/SqfsMd5Db.py
@@ -1,20 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 '''
 Created on Nov 2, 2013
 
 @author: moz
 '''
 
+
+import sqlite3 as lite
+import sys
+        
 class Md5Db:
     
     
-    def __init__(self):
-        self.Count = None
+    def __init__(self, Filename ):
+        self._count = 0
+        self._con = None
+        self._Filename = Filename
+        self._initDb(self._Filename)
+
+    def _initDb( self, DbFilename ):
+        try:
+            self._con = lite.connect('test.db')
+            
+            cur = self._con.cursor()    
+            cur.execute('SELECT SQLITE_VERSION()')
+            
+            data = cur.fetchone()
+            
+            print "SQLite version: %s" % data                
+            
+        except lite.Error, e:
+            
+            print "Error %s:" % e.args[0]
+            sys.exit(1)
+ 
     
     @property
     def Count(self):
-        return 0
+        return self._count
     
 
 
 if __name__ == '__main__':
+
     pass
--- backupscripts/unittests/testDbAccess.py
+++ backupscripts/unittests/testDbAccess.py
@@ -14,18 +14,21 @@ class Test(unittest.TestCase):
 
 
     def setUp(self):
-        self.Md5Db = Md5Db( ":memory: ")
-
+        #self.Md5Db = Md5Db( ":memory: ")
+        pass
 
     def tearDown(self):
         pass
 
+    def testInitMd5Db(self):
+        self.Md5Db = Md5Db( ":memory: ")
+        self.assertEqual(self.Md5Db.Count, 0)
 
-    def testAddMd5File(self):
-        EntriesCount = self.Md5Db.Count
-        
-        self.Md5Db.Add( Md5File )
-        self.assertEqual( Md5Db.Count, EntriesCount+Md5FileEntriesCount )
+#     def testAddMd5File(self):
+#         EntriesCount = self.Md5Db.Count
+#         
+#         self.Md5Db.Add( Md5File )
+#         self.assertEqual( Md5Db.Count, EntriesCount+Md5FileEntriesCount )
 
 
 if __name__ == "__main__":
