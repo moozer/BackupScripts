61437f71d8831ba5aa080c12ae7cce0d506de0c4
moz <moz@local>
2013-11-02 01:04:59 +0100

    add implemented

--- backupscripts/SqfsMd5Db.py
+++ backupscripts/SqfsMd5Db.py
@@ -22,7 +22,7 @@ class Md5Db:
 
     def _initDb( self, DbFilename ):
         try:
-            self._con = lite.connect('test.db')
+            self._con = lite.connect(DbFilename)
             
             cur = self._con.cursor()    
             cur.execute('SELECT SQLITE_VERSION()')
@@ -30,6 +30,8 @@ class Md5Db:
             data = cur.fetchone()
             
             print "SQLite version: %s" % data                
+            
+            cur.execute('''CREATE TABLE md5value(md5set, md5, filename)''') 
             
         except lite.Error, e:
             
@@ -39,8 +41,24 @@ class Md5Db:
     
     @property
     def Count(self):
+        cur = self._con.cursor()    
+        cur.execute('SELECT count(*) from md5value')
+        
+        data = cur.fetchone()        
+        self._count = data[0]
         return self._count
     
+
+    
+    def Add(self, Md5Filename ):
+        f = open( Md5Filename, "r")
+        cur = self._con.cursor()    
+        
+#        for md5, fileentry in f:
+        for entry in f:
+            md5, fileentry = entry.split(' ', 1 );
+            cur.execute("insert into md5value(md5set, md5, filename) values (?,?,?)",( Md5Filename, md5, fileentry));
+        pass
 
 
 if __name__ == '__main__':
--- backupscripts/unittests/testDbAccess.py
+++ backupscripts/unittests/testDbAccess.py
@@ -7,28 +7,29 @@ import unittest
 from SqfsMd5Db import Md5Db
 
 DataDir = "testdata/"
-Md5File = DataDir + "Md5FiveEntries.txt"
-Md5FileEntriesCount = 5
+Md5File = DataDir + "Md5FourEntries.txt"
+Md5FileEntriesCount = 4
 
 class Test(unittest.TestCase):
 
 
     def setUp(self):
-        #self.Md5Db = Md5Db( ":memory: ")
+        #self.Md5Db = Md5Db( ":memory:")
         pass
 
     def tearDown(self):
         pass
 
     def testInitMd5Db(self):
-        self.Md5Db = Md5Db( ":memory: ")
+        self.Md5Db = Md5Db( ":memory:")
         self.assertEqual(self.Md5Db.Count, 0)
 
-#     def testAddMd5File(self):
-#         EntriesCount = self.Md5Db.Count
-#         
-#         self.Md5Db.Add( Md5File )
-#         self.assertEqual( Md5Db.Count, EntriesCount+Md5FileEntriesCount )
+    def testAddMd5File(self):
+        self.Md5Db = Md5Db( ":memory:")
+        EntriesCount = self.Md5Db.Count
+         
+        self.Md5Db.Add( Md5File )
+        self.assertEqual( self.Md5Db.Count, EntriesCount+Md5FileEntriesCount )
 
 
 if __name__ == "__main__":
